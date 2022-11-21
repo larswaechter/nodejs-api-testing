b93aa2b656eb7b9dd0ecc3c9740fce56f5daf56d
Lars Wächter <lars97.waechter@gmail.com>
2022-11-21 21:25:40 +0100

    Fix tests

--- src/factories/repo.factory.ts
+++ src/factories/repo.factory.ts
@@ -1,10 +1,6 @@
 import { AbsTestFactory } from './abs.factory';
 
 export class RepoTestFactory extends AbsTestFactory {
-	constructor() {
-		super();
-	}
-
 	close(cb: (err?: Error) => void) {
 		this.disconnectDB(cb);
 	}
